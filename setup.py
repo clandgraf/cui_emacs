3363713e4d4d2d286dd9051c02181dfdfca41e0c
Christoph Landgraf <christoph.landgraf@gmail.com>
2017-10-18 13:00:13 +0200

    setup.py, etc

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(
+    name =         "cui_emacs",
+    version =      "0.0.1",
+    author =       "Christoph Landgraf",
+    author_email = "christoph.landgraf@googlemail.com",
+    description =  "Emacs Bindings for cui",
+    license =      "BSD",
+    url =          "https://github.com/clandgraf/cui_emacs",
+    packages =     ['cui_emacs']
+)
